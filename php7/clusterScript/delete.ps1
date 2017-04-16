181d0a15f021a4e8f57da6ff61fa97e337204c48
Grégoire <greg.joncour@gmail.com>
2017-04-16 17:24:37 +0200

    Update dockerfiles + other

---
+++ php7/clusterScript/delete.ps1
@@ -0,0 +1,10 @@
+docker rmi $(docker images -q) -f
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+
+docker-machine rm worker2 --force
+docker-machine rm worker1 --force
+docker-machine rm leader3 --force
+docker-machine rm leader2 --force
+docker-machine rm leader1 --force
